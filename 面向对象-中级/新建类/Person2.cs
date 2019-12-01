3d82987f696b0698348c8b80b4304d0708044532
shanzm <2351144690@qq.com>
2019-12-01 23:09:57 +0800

    2019年12月1日 23:09:31-update 关于类的初始化器

---
+++ 面向对象-中级/新建类/Person2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace 新建类
+{
+    class Person2
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        //给属性设置默认值
+        public string Gender { get; set; } = "女";
+
+        public void Introduct()
+        {
+            //C#6.0中using static关键字访问静态类的属性和方法，
+
+            //以前我们调用静态方法使用的是类名.静态方法，现在哦我们只要使用"using static 静态类名;"
+            //如：添加using static System.Console;就可以直接调用Console静态类中的方法
+            WriteLine($"我是{this.Name},我的年龄是{this.Age},我是一个{this.Gender},你好！");
+        }
+    }
+}
--- 面向对象-中级/新建类/Program.cs
+++ 面向对象-中级/新建类/Program.cs
@@ -7,8 +7,9 @@ namespace 新建类
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
+            //-----------------------------------------------------------------Person类
             Person p0 = new Person();//使用的是无参构造函数
             p0.Name = "志铭";
             p0.Age = 24;
@@ -20,6 +21,27 @@ namespace 新建类
 
             Person p2 = new Person("女");
             p2.Introduct();
+
+            //------------------------------------------------------------------Person2类
+            //使用的是默认的无参构造函数
+            Person2 p3 = new Person2() { Name = "p3", Age = 25, Gender = "男" };
+            p3.Introduct();
+
+            //Person2中的Gender属性的默认值是"女"
+            Person2 p4 = new Person2() { Name = "p4", Age = 25 };
+            p4.Introduct();
+
+            //只给部分属性初始化
+            Person2 p5 = new Person2() { Name = "p5" };//注意哦我们没有设置Age的默认值，也没有给Age初始化，但是C#其整形默认是0
+            p5.Introduct();//输出：我是p5,我的年龄是0，我是一个女生，你好！
+
+            //使用的类的快速初始化器
+            //注意上面的p3,p4,p5在定义的时候，使用new Person2{……}也是可以的，
+            //即：new Person2(){……};和new Person2{……};效果是一样的
+            Person2 p6 = new Person2 { Name = "p4", Age = 25, Gender = "男" };
+            p6.Introduct();
+
+            //综上我们可以发现其实我们在Person2中并没有定义构造函数，但是我们使用初始化器依旧可是实现初始化
 
             Console.ReadKey();
         }
