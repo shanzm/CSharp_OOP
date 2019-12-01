9dccc35a7a3a6c2005649261994cbef2d7ad1531
shanzm <2351144690@qq.com>
2019-12-01 22:06:05 +0800

    2019年12月1日 22:05:36-update 关于Dictionary类型的初始化器

--- 面向对象-中级/字典Dictionary/Program.cs
+++ 面向对象-中级/字典Dictionary/Program.cs
@@ -9,22 +9,44 @@ namespace 字典Dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> myDictinary = new Dictionary<int, string>();
-            myDictinary.Add(1, "a");
-            myDictinary.Add(2, "b");
-            myDictinary.Add(3, "c");
+            //新建一个字典类型并初始化
+            //Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+            //myDictinary.Add(1, "a");
+            //myDictinary.Add(2, "b");
+            //myDictinary.Add(3, "c");
+
+            //2019年12月1日 21:52:01
+            //使用快速初始化器
+            //Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            //{
+            //    { 1,"a"},
+            //    {2,"b" },
+            //    {3,"c" }
+            //};
+
+            //2019年12月1日 21:54:47
+            //C#6.0
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            {
+                [1] = "a",
+                [2] = "b",
+                [3] = "c"
+            };
+
+
+
             //覆盖式赋值
-            myDictinary[1] = "A";
+            myDictionary[1] = "A";
 
             //foreach (KeyValuePair  <int ,string> item in myDictinary )
             //{
             //    Console.WriteLine("{0}:{1}",item.Key , item.Value);
             //}
             //法二
-            foreach (int  item in myDictinary .Keys )
+            foreach (int item in myDictionary.Keys)
             {
-                Console.WriteLine("{0}:{1}", item, myDictinary[item]);
-               
+                Console.WriteLine("{0}:{1}", item, myDictionary[item]);
+
             }
             Console.ReadKey();
         }
