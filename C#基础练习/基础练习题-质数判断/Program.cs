6807e6d49ab4c43543770148b6b595385f8c55ea
shanzm <2351144690@qq.com>
2020-03-27 16:52:35 +0800

    2020年3月27日 16:51:43- Update：改进
    判断其平方根是否为质数即可

--- C#基础练习/基础练习题-质数判断/Program.cs
+++ C#基础练习/基础练习题-质数判断/Program.cs
@@ -9,9 +9,12 @@ namespace 基础练习题_质数判断
     {
         static void Main(string[] args)
         {
-           // string b = Judge(5).ToString();
-            bool b = LsPrime(5);
+            // string b = Judge(5).ToString();
+            bool b = LsPrime2(9);
             Console.WriteLine(b);//可以直接打印布尔类型变量
+
+            double a = 2.8;
+            Console.WriteLine(((int)a+1));
             Console.ReadKey();
         }
 
@@ -42,5 +45,26 @@ namespace 基础练习题_质数判断
             }
             return true;
         }
+
+        //改进：
+        //一个数若可以进行因数分解，那么分解时得到的两个数一定是一个小于等于sqrt(n)，一个大于等于sqrt(n)，
+        //据此，上述代码中并不需要遍历到n-1，遍历到sqrt(n)即可，因为若sqrt(n)左侧找不到约数，那么右侧也一定找不到约数
+        public static bool LsPrime2(int n)
+        {
+            int temp = Convert.ToInt16((Math.Sqrt(n)));
+
+            for (int i = 2; i <=temp; i++)//注意这里是“<=”
+            {
+                if (temp % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
+
+//Convert.ToInt(Object value):将Object类型转换为int类型，且四舍六入，中间取偶（比如：4.5=4,5.5=6），null取0
+//int.Parse(string value):只能将string类型转换为int类型，小数报错,null报错
+//(int):将数值类型强制转换为整型，若是小数则向左取整
