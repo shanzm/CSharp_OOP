cf179e7dbd2f18f1ec5d03acb787021b75244761
shanzm_aimu <2351144690@qq.com>
2021-07-30 16:21:28 +0800

    2021年7月30日 16:20:39-Update：一个简单的注册加密和登陆匹配的逻辑示例

--- MD5加密/Program.cs
+++ MD5加密/Program.cs
@@ -10,12 +10,43 @@ namespace MD5加密
     {
         static void Main(string[] args)
         {
-           string strs= Console.ReadLine();
-           string MD5strs=GetMD5(strs);
-           Console.WriteLine(MD5strs);
-           Console.ReadKey();
+            //string strs = "1";//Console.ReadLine();
+            //string MD5strs = GetMD5(strs);//C4CA4238A0B923820DCC509A6F75849B
+            //Console.WriteLine(MD5strs);
+
+
+            #region 密码加密
+            //用户注册逻辑：
+            //用户提供密码，以及其他的注册信息
+            //程序中的随机生成一个字符串（这个字符串作为加密的Salt）
+            //将用户密码和Salt拼接在一起
+            //将拼接后的字符串按照Md5算法加密
+            //将加密后的字符串和Slat保存在数据库
+
+            string userPwd = Console.ReadLine();//用户提供的密码
+            string salt = GetRandomString(8);
+            string signInInfo = salt + userPwd;//拼接一个Salt值
+            string password = GetMD5(signInInfo);//Md5加密
+
+            //todo:将salt和password保存在数据库中
+            Console.WriteLine($"salt+userPwd:{signInInfo}");
+            Console.WriteLine($"加密后存储到数据库的密码:{password}");
+
+            //登陆时通过用户输入的密码和该用户注册时在数据库中保存在Salt,加密后与数据库中加密的密码匹配
+            Console.WriteLine($"用户登陆时后再次加密得到的结果:{GetMD5(signInInfo)}");
+            #endregion
+
+
+
+            Console.ReadKey();
         }
 
+
+        /// <summary>
+        /// 将字符串加密为Md5
+        /// </summary>
+        /// <param name="strs">字符串</param>
+        /// <returns></returns>
         public static string GetMD5(string strs)
         {
             //创建MD5对象
@@ -30,16 +61,36 @@ namespace MD5加密
             ///1.将字节数组中的每一个元素按照指定的编码格式解析为字符串---string MD5strs = Encoding.Default.GetString(MD5buffer);
             ///2.直接将字符数组.ToString();
             ///3.将字符数组中的每一个元素.ToString();
-            
+
             //我们采用第三种方式
-            string MD5strs=null ;
+            string MD5strs = null;
             for (int i = 0; i < MD5buffer.Length; i++)
             {
-                MD5strs += MD5buffer[i].ToString ("X2");//参数X2,意味着将字符串转化为16进制
+                MD5strs += MD5buffer[i].ToString("X2");//参数X2,意味着将字符串转化为16进制
             }
-            
+
             return MD5strs;
-            
+
+        }
+
+
+
+        /// <summary>
+        /// 生成指定长度的随机数
+        /// </summary>
+        /// <param name="iLength">期望长度</param>
+        /// <returns></returns>
+        public static string GetRandomString(int iLength)
+        {
+            string buffer = "0123456789";// 随机字符中也可以为汉字（任何）
+            StringBuilder sb = new StringBuilder();
+            Random r = new Random();
+            int range = buffer.Length;
+            for (int i = 0; i < iLength; i++)
+            {
+                sb.Append(buffer.Substring(r.Next(range), 1));
+            }
+            return sb.ToString();
         }
     }
 }
